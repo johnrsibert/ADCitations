32346971b64acbc4b56fd62abe14df5dc52993f0
John Sibert <sibert@hawaii.edu>
2019-04-06 13:26:36 -0700

    clean up cite.py

--- cite.py
+++ cite.py
@@ -3,101 +3,44 @@
 import pandas as pd
 import re
 
-def ReadCitations(file_name):
-    tmp = pd.read_csv(file_name,header=3,sep='|')
-    print("Read",tmp.size,"citations from",file_name)
-    print(tmp.shape)
+def read_citations(file_name,header=3,sep='|'):
+    tmp = pd.read_csv(file_name,header=header,sep=sep)
+    print("Read",tmp.size,"citations item from",file_name)
+#   print(tmp.shape)
     tmp = tmp [['Authors', 'Publication Year', 'Title', 'Source Title', 
                  'Beginning Page', 'Ending Page', 'Volume']]
     print(tmp.shape)
-    for index,row in tmp.iterrows():
-        row['Source Title'] = str(re.sub('&',' AND ',row['Source Title'])).title()
-        print(row['Source Title'])
-        row['Authors'] = re.sub(';', ' AND ', row['Authors'])
-        print(row['Authors'])
 
-#   tmp = tmp.drop_duplicates(keep='last')
     return tmp
 
 
-def WriteBibTex(f,art='X'):
-    print("f:",f.shape)
-    bib_file = open(art + str("_citations.bib"),'w')
+def write_bibtex(ff,art='X'):
+    s1 = ff.shape[0]
+    f = ff.drop_duplicates(keep='last')
+    s2 = f.shape[0]
+    dd = s1-s2
+    print((s1-s2),"duplicates dropped")
+
+    bib_file = open("AD_citations.bib",'w')
     for count,row in f.iterrows():
-    #   item = "@article{art%04i,\n"%count
         item = "@article{%s%04i,\n"%(art,(count+1))
+        row['Authors'] = re.sub(';', ' AND ', row['Authors'])
         item += "   author = {%s},\n"%row['Authors']
         item += "   year = {%s},\n"%row['Publication Year']
         item += "   title = {%s},\n"%row['Title']
+        row['Source Title'] = str(re.sub('&',' AND ',row['Source Title'])).title()
         item += "   journal = {%s},\n"%row['Source Title']
-        item += "   pages = {%.0f-%.0f},\n"%(row['Beginning Page'],row['Ending Page'])
-    #   item += "   pages = {%.0f-"%row['Beginning Page']
-    #   item += "%.0f},\n"%row['Ending Page']
-        item += "   volume = {%s},\n"%row['Volume']
+        if pd.notna(row['Beginning Page']):
+            item += "   pages = {%.0f -- %.0f},\n"%(row['Beginning Page'],row['Ending Page'])
+        item += "   volume = {%s}\n"%row['Volume']
         item += "}\n"
     #   print(item)
         bib_file.write(item) 
 
 
 
-citations = ReadCitations("T_citations.csv")
+citations = read_citations("T_citations.csv")
 print(citations.tail)
-WriteBibTex(citations)
+print(citations.shape[0])
+write_bibtex(citations)
 
-#file_name = "T_citations.csv"
-#print("Hello world! I'm trying to read ",file_name)
-#
-#
-#print(tmp.ndim)
-#print(tmp.shape)
-#print(tmp.size)
-#print(tmp.index)
-#print(tmp.columns)
-#tmp1 = tmp [['Authors', 'Publication Year', 'Title', 'Source Title', 
-#             'Beginning Page', 'Ending Page', 'Volume']]
-#print(tmp1.ndim)
-#print(tmp1.shape)
-#print(tmp1.size)
-#print(tmp1.index)
-#print(tmp1.columns)
-##print(tmp.head())
-##print(tmp.tail())
-##print(tmp['Volume'])
-#for index,row in tmp1.iterrows():
-#    print (row['Source Title'])
-#    row['Source Title'] = str(re.sub('&'," AND ",row['Source Title'])).title()
-#    print (row['Source Title'])
-##   journal = str(row['Source Title'])
-##   print(index,journal)
-##   journal = re.sub('&'," AND ",journal)
-##   print(index,journal.title())
-#
-#    print(row['Authors'])
-#    row['Authors'] = re.sub(';', ' AND ', row['Authors'])
-#    print(row['Authors'])
-##   print(author)
-##   author = re.sub(';', ' AND ', author)
-##   print(author)
-#
-#    tmp2 = tmp1.drop_duplicates(keep='last')
-#    print(tmp.shape)
-#    print(tmp.size)
-#    print(tmp1.shape)
-#    print(tmp1.size)
-#    print(tmp2.shape)
-#    print(tmp2.size)
-#
-#
-#
-##Index(['Title', 'Authors', 'Corporate Authors', 'Editors', 'Book Editors',
-##       'Source Title', 'Publication Date', 'Publication Year', 'Volume',
-##       'Issue', 'Part Number', 'Supplement', 'Special Issue', 'Beginning Page',
-##       'Ending Page', 'Article Number', 'DOI', 'Conference Title',
-##       'Conference Date', 'Total Citations', 'Average per Year', '1980',
-##       '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989',
-##       '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998',
-##       '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007',
-##       '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
-##       '2017', '2018', '2019'],
-##      dtype='object')
-##
